c90a39e6c0448c3c1fe1a3535bea12e47b30dd35
Michael Williams <michael.williams@enspiral.com>
2019-04-27 14:37:30 +1200

    add better example

--- README.md
+++ README.md
@@ -31,6 +31,47 @@
   </a>
 </div>
 
+## Example
+
+```rust
+use nest::{Store, Value};
+
+use serde_json::json;
+
+fn main() {
+    // what is the root path to your data store?
+    let root = "./";
+    // describe how your data store will map to the filesystem
+    let schema = json!({
+        // refers to a directory: ./example-data/
+        "example-data": {
+            // refers to a file: ./example-data/foo.json
+            "foo": "json"
+        }
+    }).into();
+
+    let store = Store::new(root, schema);
+
+    // get `bar` key from `./example-data/foo.json` file
+    let bar = store.get(&["example-data", "foo", "bar"]).unwrap();
+    println!("bar {:?}", bar);
+
+    // set `bar` key in `./example-data/foo.json` file
+    let next_bar = &Value::String("baz".into());
+    store.set(&["example-data", "foo", "bar"], next_bar).unwrap();
+
+    // get a sub-store for data within `./example-data/foo.json
+    let foo = store.sub(&["example-data", "foo"]).unwrap();
+    let bar = foo.get(&["bar"]).unwrap();
+    println!("bar {:?}", bar);
+
+    // try to get a value that doesn't map to the schema
+    let err = store.get(&["invalid", "path"]);
+    println!("err {:?}", err);
+}
+```
+
+
 ## Contributing
 
 ### Conduct
--- nest-cli/src/main.rs
+++ nest-cli/src/main.rs
@@ -3,33 +3,33 @@ use nest::{Store, Value};
 use serde_json::json;
 
 fn main() {
+    // what is the root path to your data store?
+    let root = "./";
+    // describe how your data store will map to the filesystem
     let schema = json!({
+        // refers to a directory: ./example-data/
         "example-data": {
+            // refers to a file: ./example-data/foo.json
             "foo": "json"
         }
     }).into();
-    let root = "./";
-    let store = Store::new(schema, root);
 
-    // TODO change to use &str's
-    let foobar = store.get(&["example-data", "foo", "bar"]).unwrap();
-    println!("foobar {:?}", foobar);
+    let store = Store::new(root, schema);
 
-    store.set(&["example-data", "foo", "bar"], &Value::String("hello".into())).unwrap();
+    // get `bar` key from `./example-data/foo.json` file
+    let bar = store.get(&["example-data", "foo", "bar"]).unwrap();
+    println!("bar {:?}", bar);
 
-    let ab = store.get(&["a", "b"]);
-    println!("ab {:?}", ab);
+    // set `bar` key in `./example-data/foo.json` file
+    let next_bar = &Value::String("baz".into());
+    store.set(&["example-data", "foo", "bar"], next_bar).unwrap();
+
+    // get a sub-store for data within `./example-data/foo.json
+    let foo = store.sub(&["example-data", "foo"]).unwrap();
+    let bar = foo.get(&["bar"]).unwrap();
+    println!("bar {:?}", bar);
+
+    // try to get a value that doesn't map to the schema
+    let err = store.get(&["invalid", "path"]);
+    println!("err {:?}", err);
 }
-
-// dyndns-host
-// config = db.sub(['dyndns', 'host'])
-// config.get(['guests'])
-// config.set(['guests'], 'butt')
-
-// mix = db.get(['humans', 'mix'])
-// mix.get()
-// mix.set({ name: 'Mix' })
-//
-// db.set(['humans', 'mix'], { name: 'Mix' })
-//
-// immutable-rs
