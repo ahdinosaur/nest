2137ca55ce5a431e58808101158efb09603828ca
Michael Williams <michael.williams@enspiral.com>
2019-04-27 19:35:52 +1200

    start working on nest-cli

--- nest-cli/src/main.rs
+++ nest-cli/src/main.rs
@@ -1,37 +1,104 @@
-use nest::{Store, Error, Value};
+#[macro_use]
+extern crate log;
 
-use serde_json::json;
+use clap_verbosity_flag;
+use clap_log_flag;
+use nest::{Error, Store, Schema, Value};
+use serde_json as json;
+use std::env;
+use std::fs::read_to_string;
+use std::io::{self, Read};
+use std::path::{PathBuf};
+use structopt::StructOpt;
 
-fn main () -> Result<(), Error> {
-    // what is the root path to your data store?
-    let root = "./";
-    // describe how your data store will map to the filesystem
-    let schema = json!({
-        // refers to a directory: ./example-data/
-        "example-data": {
-            // refers to a file: ./example-data/foo.json
-            "foo": "json"
-        }
-    }).into();
+#[derive(Debug, StructOpt)]
+#[structopt(name = "nest",
+            rename_all = "kebab-case",
+            long_about = "\nUse your filesystem as a nested data store!",
+            raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+struct Cli {
+  #[structopt(subcommand)]
+  command: Command,
+  #[structopt(flatten)]
+  log: clap_log_flag::Log,
+  #[structopt(long = "root", parse(from_os_str))]
+  root: Option<PathBuf>,
+  #[structopt(flatten)]
+  verbose: clap_verbosity_flag::Verbosity,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(name = "get")]
+    /// Get value from Nest.
+    Get {
+        path: String,
+    },
+
+    #[structopt(name = "set")]
+    /// Set value in Nest.
+    Set {
+        path: String,
+        value: Option<String>
+    },
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+
+    args.log.log_all(Some(args.verbose.log_level()))?;
+
+    info!("nest-cli: {}", env::var("CARGO_PKG_VERSION")?);
+    debug!("args: {:#?}", args);
+
+    let root = match args.root {
+        Some(path) => path,
+        None => env::var("NEST_ROOT").map(|s| s.into())
+            .or(env::current_dir())
+            ?,
+    };
+
+    debug!("root: {:#?}", root);
+    let schema_path = root.join(".nest.json");
+    debug!("schema path: {:#?}", schema_path);
+    if !schema_path.is_file() {
+        error!("schema path is not a file: {:#?}", schema_path);
+        return Ok(());
+    }
+    let schema_str = read_to_string(schema_path)?;
+    debug!("schema string: {:#?}", schema_str);
+    let schema_json: json::Value = json::from_str(&schema_str)?;
+    debug!("schema json: {:#?}", schema_json);
+    let schema: Schema = schema_json.into();
+    debug!("schema: {:#?}", schema);
 
     let store = Store::new(root, schema);
 
-    // get `bar` key from `./example-data/foo.json` file
-    let bar = store.get(&["example-data", "foo", "bar"])?;
-    println!("bar {:?}", bar);
+    match args.command {
+        Command::Get { path } => {
+            let path: Vec<&str> = path.split('/').collect();
+            let value = store.get(&path)?;
+            let value_json: json::Value = value.into();
+            let value_string = json::to_string_pretty(&value_json)?;
+            println!("{}", value_string);
+        },
+        Command::Set { path, value } => {
+            let path: Vec<&str> = path.split('/').collect();
+            let value_str: String = match value {
+                Some(value_string) => value_string,
+                None => {
+                    let mut string = String::new();
+                    let stdin = io::stdin();
+                    let mut handle = stdin.lock();
+                    handle.read_to_string(&mut string)?;
+                    string
+                },
+            };
+            let value_json: json::Value = json::from_str(&value_str)?;
+            let value: Value = value_json.into();
+            store.set(&path, &value)?;
+        }
+    }
 
-    // set `bar` key in `./example-data/foo.json` file
-    let next_bar = &Value::String("baz".into());
-    store.set(&["example-data", "foo", "bar"], next_bar)?;
-
-    // get a sub-store for data within `./example-data/foo.json
-    let foo = store.sub(&["example-data", "foo"])?;
-    let bar = foo.get(&["bar"])?;
-    println!("bar {:?}", bar);
-
-    // try to get a value that doesn't map to the schema
-    let err = store.get(&["invalid", "path"]);
-    println!("err {:?}", err);
-
-    Ok(())
+  Ok(())
 }
