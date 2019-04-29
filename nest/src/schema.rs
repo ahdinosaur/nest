3da095e5a0c2ae369ce127968887b3b9487a3f35
Michael Williams <michael.williams@enspiral.com>
2019-04-29 18:28:47 +1200

    try to fix Into<Result<T>> but yeah nah

--- nest/src/schema.rs
+++ nest/src/schema.rs
@@ -1,10 +1,11 @@
 use std::collections::BTreeMap;
 use std::convert::From;
 use std::iter::FromIterator;
+use std::ops::Deref;
 
 use serde_json as json;
 
-// use crate::error::Error;
+use crate::error::Error;
 use crate::source::{self, Source};
 use crate::value::Value;
 
@@ -32,27 +33,39 @@ pub enum Schema {
 // - wait until [re-rebalancing-coherence] feature is stable:
 //     https://github.com/mssun/state-of-rust/blob/master/stable_library_feature.txt
 
-impl From<Value> for Schema {
-    fn from(value: Value) -> Schema {
-        match value {
-            Value::Object(object) => Schema::Directory(BTreeMap::from_iter(
+impl From<Value> for WrappedResult<Schema> {
+    fn from(value: Value) -> WrappedResult<Schema> {
+        WrappedResult(match value {
+            Value::Object(object) => Ok(Schema::Directory(BTreeMap::from_iter(
                 object
                     .into_iter()
-                    .map(|(key, value)| (key, Self::from(value))),
-            )),
+                    .map(|(key, value)| (key, Self::from(value)?)),
+            ))),
             Value::String(string) => match string.as_str() {
-                "json" => Schema::Source(Box::new(source::Json {})),
-                "hjson" => Schema::Source(Box::new(source::Hjson {})),
-                _ => panic!("Invalid string in json Schema: {:?}", string),
+                "json" => Ok(Schema::Source(Box::new(source::Json {}))),
+                "hjson" => Ok(Schema::Source(Box::new(source::Hjson {}))),
+                _ => Err(Error::Unexpected),
+                // _ => panic!("Invalid string in json Schema: {:?}", string),
             },
-            _ => panic!("Invalid value in json Schema: {:?}", value),
-        }
+            _ => Err(Error::Unexpected),
+            // _ => panic!("Invalid value in json Schema: {:?}", value),
+        })
     }
 }
 
-impl From<json::Value> for Schema {
-    fn from(value: json::Value) -> Schema {
+impl From<json::Value> for WrappedResult<Schema> {
+    fn from(value: json::Value) -> WrappedResult<Schema> {
         let value: Value = value.into();
         value.into()
+    }
+}
+
+struct WrappedResult<T>(Result<T, Error>);
+
+impl<T> Deref for WrappedResult<T> {
+    type Target = Result<T, Error>;
+
+    fn deref(&self) -> &Result<T, Error> {
+        &self.0
     }
 }
